1f82d528c515ef3f2bd2188376cdbde93add2e53
Kasan <114809701+Shanove7@users.noreply.github.com>
2026-02-08 00:00:08 +0700

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// credits : kasan
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
